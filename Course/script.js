920cc3b309e15ea3890ffd2eb7b67c9d9d2d2d25
Arman <arm167167@gmail.com>
2020-04-01 17:58:48 +0300

    first commit

---
+++ Course/script.js
@@ -0,0 +1,72 @@
+// 'use strict';
+
+
+
+var number = 5;
+var string = "Hello";
+var sym = Symbol();
+var boolean = true;
+// null;
+// undefined;
+var obj = {};
+
+console.log(4/0);
+console.log('string'*9);
+
+let something;
+console.log(something);
+
+
+let person = {
+    name: "John",
+    age: 25,
+    isMarried: false
+
+};
+
+//console.log(person.name);
+// console.log(person["name"]);
+
+let arr = ['plum.png', 'orange.jpg' , 'apple.bmp'];
+console.log(arr[2]);
+
+// alert("hello world");
+
+// let answere = confirm("Are you here?");
+// console.log(answere);
+
+// let answere = +prompt("Are you over 18?", "Yes");
+// console.log(typeof(arr));
+
+// console.log(typeof(answere));
+
+// console.log("arr" + " - object");
+// console.log(4 + " - object");
+
+let incr = 10,
+    decr = 10;
+
+
+// console.log(++incr);
+// console.log(--decr);
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" == 2);
+
+
+let isChecked = true,
+    isClose = false;
+
+console.log(isChecked || isClose);
+
+
+
+
+
+
+
+
+
