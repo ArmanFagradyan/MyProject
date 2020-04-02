18714e44ea25891b28d135d10bc7c9fdfa4175e5
Arman <arm167167@gmail.com>
2020-04-03 00:10:48 +0300

    JS changes

--- Course/script.js
+++ Course/script.js
@@ -2,70 +2,102 @@
 
 
 
-var number = 5;
-var string = "Hello";
-var sym = Symbol();
-var boolean = true;
-// null;
-// undefined;
-var obj = {};
+// var number = 5;
+// var string = "Hello";
+// var sym = Symbol();
+// var boolean = true;
+// // null;
+// // undefined;
+// var obj = {};
 
-console.log(4/0);
-console.log('string'*9);
+// console.log(4/0);
+// console.log('string'*9);
 
-let something;
-console.log(something);
+// let something;
+// console.log(something);
 
 
-let person = {
-    name: "John",
-    age: 25,
-    isMarried: false
+// let person = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false
 
+// };
+
+// //console.log(person.name);
+// // console.log(person["name"]);
+
+// let arr = ['plum.png', 'orange.jpg' , 'apple.bmp'];
+// console.log(arr[2]);
+
+// // alert("hello world");
+
+// // let answere = confirm("Are you here?");
+// // console.log(answere);
+
+// // let answere = +prompt("Are you over 18?", "Yes");
+// // console.log(typeof(arr));
+
+// // console.log(typeof(answere));
+
+// // console.log("arr" + " - object");
+// // console.log(4 + " - object");
+
+// let incr = 10,
+//     decr = 10;
+
+
+// // console.log(++incr);
+// // console.log(--decr);
+
+// console.log(incr++);
+// console.log(decr--);
+
+// console.log(5%2);
+// console.log("2" == 2);
+
+
+// let isChecked = true,
+//     isClose = false;
+
+// console.log(isChecked || isClose);
+
+
+
+// if (2*4 == 9) {
+//     console.log("Right")
+// } else{
+//     console.log("Not Right");
+// }
+
+let num = 50;
+
+if (num < 49) {
+    console.log("Not right");
+} else if (num > 100) {
+    console.log("Too much");
+} else {
+    console.log("Right");
 };
 
-//console.log(person.name);
-// console.log(person["name"]);
+(num == 50)? console.log('Right') : console.log('Wrong');
 
-let arr = ['plum.png', 'orange.jpg' , 'apple.bmp'];
-console.log(arr[2]);
-
-// alert("hello world");
-
-// let answere = confirm("Are you here?");
-// console.log(answere);
-
-// let answere = +prompt("Are you over 18?", "Yes");
-// console.log(typeof(arr));
-
-// console.log(typeof(answere));
-
-// console.log("arr" + " - object");
-// console.log(4 + " - object");
-
-let incr = 10,
-    decr = 10;
-
-
-// console.log(++incr);
-// console.log(--decr);
-
-console.log(incr++);
-console.log(decr--);
-
-console.log(5%2);
-console.log("2" == 2);
-
-
-let isChecked = true,
-    isClose = false;
-
-console.log(isChecked || isClose);
-
-
-
-
-
+switch (num) {
+    case num < 49:
+        console.log('Right');
+        break;
+    case num > 100:
+        console.log('Too much');
+        break;
+    case num > 80:
+        console.log('Still too much');
+        break;
+    case 50:
+        console.log('Right');
+        break;
+    default:
+        console.log('Something went wrong');
+}
 
 
 
