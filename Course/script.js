ffe84c44cf182cffc23970e8898a5017d1ded4da
Arman <arm167167@gmail.com>
2020-04-07 22:03:51 +0300

    Another Lesson

--- Course/script.js
+++ Course/script.js
@@ -249,3 +249,18 @@ function compareNum(a, b){
 
 console.log(i);
 
+let soldier = {
+    health: 400,
+    armor: 100
+};
+
+let John = {
+    health: 100
+};
+
+John.__proto__ = soldier;
+console.log(John);
+console.log(John.armor);
+
+
+
