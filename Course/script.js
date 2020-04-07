e14e86a9f73b3d353f91f17c390ac140a14e3f76
Arman <arm167167@gmail.com>
2020-04-07 05:14:20 +0300

    Half of the new lesson

--- Course/script.js
+++ Course/script.js
@@ -215,4 +215,14 @@ for (let key in options) {
 
 console.log(Object.keys(options).length);
 
+let arr = [1, 2, 3, 4, 5];
 
+// for (let i = 0; i < arr.length; i++){
+//     console.log(arr[i]);
+// }
+
+arr.forEach(function(item, i, massive){
+    console.log(i + ': ' + item + " (массив: " + massive + ')');
+});
+
+console.log(arr);
