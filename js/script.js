bc624b60e024581329dbada195f1c621c8778030
Arman <arm167167@gmail.com>
2020-04-12 03:00:19 +0300

    Another lesson

--- js/script.js
+++ js/script.js
@@ -1,11 +1,37 @@
-let btn = document.getElementsByTagName('button');
+let btn = document.querySelectorAll('button'),
+    wrap = document.querySelector('.wrapper'),
+    link = document.querySelector('a');
 
 // Хотим чтобы на клик выводился алерт
 
-btn[0].onclick = function(){
-    alert('Вы нажали паервую кнопку');
-};
+// btn[0].onclick = function(){
+//     alert('Вы нажали на первую кнопку');
+// };
 
-btn[0].onclick = function(){
-    alert('Вы нажали паервую кнопку');
-};
+// btn[0].onclick = function(){
+//     alert('Вы нажали на первую кнопку');
+// };
+
+
+btn[0].addEventListener('click', function(event){
+    console.log('Произошло событие: ' + event.type + 'на элементе' + event.target);
+});
+
+wrap.addEventListener('click', function(){
+    console.log('Произошло событие: ' + event.type + 'на элементе' + event.target);
+});
+
+link.addEventListener('click', function(event){
+    event.preventDefault();
+    console.log('Произошло событие: ' + event.type + 'на элементе' + event.target);
+});
+
+btn.forEach(function(item){
+    item.addEventListener('mouseleave', function(){
+        console.log("Вышли!");
+    });
+});
+
+// btn[0].addEventListener('mouseenter', function(){
+//     alert('Вы навели на первую кнопку');
+// });
