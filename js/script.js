64b486fe77230bd2c2f8e75b8c6925354f6a557c
Arman <arm167167@gmail.com>
2020-08-25 20:31:26 +0300

    New lesson

--- js/script.js
+++ js/script.js
@@ -1,24 +1,44 @@
 "use strict";
 
-let btn = document.querySelectorAll('button'),
-    wrap = document.querySelector('.wrapper'),
-    link = document.querySelector('a');
 
-btn[0].addEventListener('click', function(event){
-    console.log('Произошло событие: ' + event.type + 'на элементе' + event.target);
+const box = document.getElementById('box');
+console.log(box);
+
+const btns = document.getElementsByTagName('button');
+console.log(btns[1]);
+
+const circles = document.getElementsByClassName('circle');
+console.log(circles);
+
+const hearts = document.querySelectorAll('.heart');
+
+hearts.forEach(item => {
+    console.log(item);
 });
 
-wrap.addEventListener('click', function(){
-    console.log('Произошло событие: ' + event.type + 'на элементе' + event.target);
-});
+const oneHeart = document.querySelector('.heart');
+console.log(oneHeart);
 
-link.addEventListener('click', function(event){
-    event.preventDefault();
-    console.log('Произошло событие: ' + event.type + 'на элементе' + event.target);
-});
 
-btn.forEach(function(item){
-    item.addEventListener('mouseleave', function(){
-        console.log("Вышли!");
-    });
-});
+// let btn = document.querySelectorAll('button'),
+//     wrap = document.querySelector('.wrapper'),
+//     link = document.querySelector('a');
+
+// btn[0].addEventListener('click', function(event){
+//     console.log('Произошло событие: ' + event.type + 'на элементе' + event.target);
+// });
+
+// wrap.addEventListener('click', function(){
+//     console.log('Произошло событие: ' + event.type + 'на элементе' + event.target);
+// });
+
+// link.addEventListener('click', function(event){
+//     event.preventDefault();
+//     console.log('Произошло событие: ' + event.type + 'на элементе' + event.target);
+// });
+
+// btn.forEach(function(item){
+//     item.addEventListener('mouseleave', function(){
+//         console.log("Вышли!");
+//     });
+// });
