bef7495c9b9b2ac540bb56891d3731ed8b639069
Arman <arm167167@gmail.com>
2020-08-28 22:03:25 +0300

    New lesson

--- js/script.js
+++ js/script.js
@@ -1,25 +1,5 @@
 "use strict";
 
-
-const box = document.getElementById('box');
-console.log(box);
-
-const btns = document.getElementsByTagName('button');
-console.log(btns[1]);
-
-const circles = document.getElementsByClassName('circle');
-console.log(circles);
-
-const hearts = document.querySelectorAll('.heart');
-
-hearts.forEach(item => {
-    console.log(item);
-});
-
-const oneHeart = document.querySelector('.heart');
-console.log(oneHeart);
-
-
 // let btn = document.querySelectorAll('button'),
 //     wrap = document.querySelector('.wrapper'),
 //     link = document.querySelector('a');
