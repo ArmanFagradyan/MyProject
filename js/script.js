5d0f82d64ca175f0f8f7fa8f57d762f071625024
Arman <arm167167@gmail.com>
2020-09-12 15:39:35 +0300

    MinorCorrections

--- js/script.js
+++ js/script.js
@@ -1,24 +1,24 @@
 "use strict";
 
-// let btn = document.querySelectorAll('button'),
-//     wrap = document.querySelector('.wrapper'),
-//     link = document.querySelector('a');
+let btn = document.querySelectorAll('button'),
+    wrap = document.querySelector('.wrapper'),
+    link = document.querySelector('a');
 
-// btn[0].addEventListener('click', function(event){
-//     console.log('Произошло событие: ' + event.type + 'на элементе' + event.target);
-// });
+btn[0].addEventListener('click', function(event){
+    console.log('Произошло событие: ' + event.type + 'на элементе' + event.target);
+});
 
-// wrap.addEventListener('click', function(){
-//     console.log('Произошло событие: ' + event.type + 'на элементе' + event.target);
-// });
+wrap.addEventListener('click', function(){
+    console.log('Произошло событие: ' + event.type + 'на элементе' + event.target);
+});
 
-// link.addEventListener('click', function(event){
-//     event.preventDefault();
-//     console.log('Произошло событие: ' + event.type + 'на элементе' + event.target);
-// });
+link.addEventListener('click', function(event){
+    event.preventDefault();
+    console.log('Произошло событие: ' + event.type + 'на элементе' + event.target);
+});
 
-// btn.forEach(function(item){
-//     item.addEventListener('mouseleave', function(){
-//         console.log("Вышли!");
-//     });
-// });
+btn.forEach(function(item){
+    item.addEventListener('mouseleave', function(){
+        console.log("Вышли!");
+    });
+});
