e61f97cf4c840b0bc74b6ba0ba914088a8a5f12e
Arman <arm167167@gmail.com>
2021-07-13 01:57:28 +0300

    Скрипты и время их выполнения. setTimeout и setInterval

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
-const btns = document.querySelectorAll('button'),
-      wrapper = document.querySelector('.btn-block');
+// const btns = document.querySelectorAll('button'),
+//       wrapper = document.querySelector('.btn-block');
 
 
 // console.log(btns[0].classList.length);
@@ -12,23 +12,23 @@ const btns = document.querySelectorAll('button'),
 //     console.log('red');
 // }
 
-btns[0].addEventListener('click', () => {
-    if (!btns[1].classList.contains('red')) {
-        btns[1].classList.add('red');
-    } else {
-        btns[1].classList.remove('red');
-    }
-});
+// btns[0].addEventListener('click', () => {
+//     if (!btns[1].classList.contains('red')) {
+//         btns[1].classList.add('red');
+//     } else {
+//         btns[1].classList.remove('red');
+//     }
+// });
 
 // wrapper.addEventListener('click', (event) => {
 //     console.dir(event.target);
 // });
 
-wrapper.addEventListener('click', (event) => {
-    if (event.target && event.target.tagName == "BUTTON") {
-        console.log('Hello');
-    }
-});
+// wrapper.addEventListener('click', (event) => {
+//     if (event.target && event.target.tagName == "BUTTON") {
+//         console.log('Hello');
+//     }
+// });
 
 // btns.forEach(btn => {
 //     btn.addEventListener('click', () => {
@@ -36,6 +36,46 @@ wrapper.addEventListener('click', (event) => {
 //     });
 // });
 
-const btn = document.createElement('button');
-btn.classList.add('red');
-wrapper.append(btn);
+// const btn = document.createElement('button');
+// btn.classList.add('red');
+// wrapper.append(btn);
+
+
+
+
+const btn = document.querySelector('.btn');
+let timerId,
+    i = 0;
+
+function myAnimation() {
+    const element = document.querySelector('.box');
+    let pos = 0;
+
+    const id = setInterval(frame, 100);
+    function frame() {
+        if (pos == 300) {
+            clearInterval(id);
+        } else { 
+            pos++;
+            element.style.top = pos + "px";
+            element.style.left = pos + "px"; 
+        }
+    }
+}
+
+btn.addEventListener('click', myAnimation);
+
+
+// function logger () {
+//     if (i === 3) {
+//         clearInterval(timerId);
+//     }
+//     console.log('text');
+//     i++;
+// }
+
+
+// let id = setTimeout(function log(){
+//     console.log('Hello');
+//     id = setTimeout(log, 500);
+// }, 500);
