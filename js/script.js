2047e3f1ff4150447b38f0a06005882b3d46dc46
Arman <arm167167@gmail.com>
2021-07-19 13:32:27 +0300

    Функции-конструкторы

--- js/script.js
+++ js/script.js
@@ -105,3 +105,30 @@
 
 // let end = new Date();
 // console.log(`Цикл отработал за ${end - start} милисекунд`);
+
+const num = new Function(3);
+console.log(num);
+
+function User(name, id) {
+    this.name = name;
+    this.id = id;
+    this.human = true;
+    this.hello = function() {
+        console.log(`Hello ${this.name}`);
+    };
+}
+
+User.prototype.exit = function(name) {
+    console.log(`User ${this.name} left`);
+};
+
+const ivan = new User('Ivan', 28);
+const alex = new User('Alex', 20);
+
+ivan.exit();
+
+ivan.hello();
+alex.hello();
+
+console.log(ivan);
+console.log(alex);
